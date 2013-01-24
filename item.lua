832028eb352f574f375ebe89e5346c091569cb67
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-01-23 21:32:35 -0800

    Adding item support.

---
+++ item.lua
@@ -0,0 +1,61 @@
+
+-- Helper functions for manipulating and transversing items.
+
+-- Our object.
+AOMRift.Item = {
+  detail = {},
+  category = {},
+  -- Copy of detail object.
+  id = nil,
+  name = nil,
+  icon = nil,
+}
+
+--
+-- Determine if item identifier is valid or not.
+--
+-- @param mixed item_id
+--   Can be any data type containing any value, including nil.
+--
+-- @return
+--   (bool) true or false.
+--
+function AOMRift.Item.Exists(item_id)
+  local report = false
+  if type(item_id) == "string" then 
+    local item = Inspect.Item.Detail(item_id)
+    if type(item) == "table" then
+      report = true
+    end
+  end
+  return report
+end
+
+--
+-- Load up AOMRift.Item object with a currency.
+--
+-- @param string item_id
+--   Rift item_id to load.
+--
+-- @return
+--   (AOMRift.Item) self contained object with item data.
+--   or nil if there was an error loading.
+--
+function AOMRift.Item:Load(item_id)
+  if AOMRift.Item.Exists(item_id) == nil then
+    return nil
+  end
+  -- Create a self contained object.
+  local o = {}
+  setmetatable(o, self)
+  self.__index = self
+
+  o.detail = Inspect.Item.Detail(item_id)
+  -- Map detail record to base object.
+  o.id   = o.detail.id
+  o.name = o.detail.name
+  o.icon = o.detail.icon or "Data/\\UI\\item_icons\\loot_platinum_coins.dds"
+  o.description = o.detail.description or "none"
+  
+  return o
+end
